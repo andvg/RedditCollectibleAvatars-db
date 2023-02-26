a3712e11f61ae62f5cd70dbfaae4b6f4daea81dd
andvg <33574513+andvg@users.noreply.github.com>
2023-02-27 00:28:39 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,41 @@
-import streamlit as st
 import pandas as pd
-import numpy as np
-import json
+import streamlit as st
 
-with open('data.json', 'r') as f:
+with open('database.json') as f:
     data = json.load(f)
 
-df = pd.DataFrame(data['data'])
+df = pd.json_normalize(data['data'])
 
-# spacchetta le colonne che contengono liste
-df = pd.concat([df.drop(['tags'], axis=1), df['tags'].apply(pd.Series)], axis=1)
-
-st.title("Analisi del database JSON")
-st.multiselect("Seleziona le colonne da visualizzare", df.columns)
-
-# Mostra la tabella dei dati
 st.dataframe(df)
+
+filters = st.sidebar.multiselect('Seleziona le colonne', df.columns)
+filtered_df = df[filters]
+st.dataframe(filtered_df)
+
+chart_type = st.sidebar.selectbox('Seleziona il tipo di grafico', ['line', 'bar', 'area'])
+
+if chart_type == 'line':
+    st.line_chart(filtered_df)
+elif chart_type == 'bar':
+    st.bar_chart(filtered_df)
+else:
+    st.area_chart(filtered_df)
+
+sort_by = st.sidebar.selectbox('Seleziona la colonna da ordinare', filtered_df.columns)
+sort_order = st.sidebar.selectbox('Seleziona l\'ordine', ['crescente', 'decrescente'])
+
+if sort_order == 'crescente':
+    sorted_df = filtered_df.sort_values(sort_by)
+else:
+    sorted_df = filtered_df.sort_values(sort_by, ascending=False)
+
+st.dataframe(sorted_df)
+
+if st.sidebar.button('Esporta in CSV'):
+    csv = filtered_df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="filtered_data.csv">Download CSV file</a>'
+    st.markdown(href, unsafe_allow_html=True)
+
+st.write('Statistiche del dataframe:')
+st.write(filtered_df.describe())

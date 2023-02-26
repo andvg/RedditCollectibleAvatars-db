60b20c57b3c722bd739184b5f947ecc7bb8e7885
andvg <33574513+andvg@users.noreply.github.com>
2023-02-27 00:20:46 +0100

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,11 @@ with open('data.json', 'r') as f:
 
 df = pd.DataFrame(data['data'])
 
+# spacchetta le colonne che contengono json
+df = pd.concat([df.drop(['data'], axis=1), df['data'].apply(pd.Series)], axis=1)
+
 st.title("Analisi del database JSON")
+st.multiselect("Seleziona le colonne da visualizzare", df.columns)
 
 # Mostra la tabella dei dati
 st.dataframe(df)
